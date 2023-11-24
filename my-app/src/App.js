7e9400a91cf1579e49cbb10d1c1af43ecd945041
kei-prog <kei08prog@gmail.com>
2023-11-25 02:47:04 +0900

    feat todo-list

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,88 @@
-import logo from './logo.svg';
-import './App.css';
+import { InputTodo } from "./components/InputTodo";
+import { TodoList } from "./components/TodoList";
+import { TodoCounts } from "./components/TodoCounts";
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [inputTodo, setInputTodo] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  const toggleComplete = (index) => {
+    const newTodos = todos.map((todo, i) => {
+      if (i === index) {
+        return { ...todo, isCompleted: !todo.isCompleted };
+      }
+      return todo;
+    });
+    setTodos(newTodos);
+  };
+
+  const onClickAdd = () => {
+    if (inputTodo === "") return;
+    const newTodos = [
+      ...todos,
+      { text: inputTodo, isCompleted: false, isEditing: false },
+    ];
+    setTodos(newTodos);
+    setInputTodo("");
+  };
+
+  const onClickDelete = (index) => {
+    if (window.confirm("本当に削除してもよろしいですか?") === false) return;
+    const newTodos = todos.filter((_todo, i) => i !== index);
+    setTodos(newTodos);
+  };
+
+  const onClickComplete = (index) => {
+    toggleComplete(index);
+  };
+
+  const onClickEdit = (index) => {
+    const newTodos = todos.map((todo, i) => {
+      if (i === index) {
+        return { ...todo, isEditing: !todo.isEditing };
+      }
+      return todo;
+    });
+    setTodos(newTodos);
+  };
+
+  const handleEditInputChange = (e, index) => {
+    const newTodos = todos.map((todo, i) => {
+      if (i === index) {
+        return { ...todo, text: e.target.value };
+      }
+      return todo;
+    });
+    setTodos(newTodos);
+  };
+
+  const totalTodosCount = todos.length;
+  const completedTodosCount = todos.filter((todo) => todo.isCompleted).length;
+  const incompleteTodosCount = totalTodosCount - completedTodosCount;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <InputTodo
+        inputTodo={inputTodo}
+        setInputTodo={setInputTodo}
+        onClickAdd={onClickAdd}
+      />
+      <div>
+        <TodoList
+          todos={todos}
+          onClickComplete={onClickComplete}
+          handleEditInputChange={handleEditInputChange}
+          onClickEdit={onClickEdit}
+          onClickDelete={onClickDelete}
+        />
+        <TodoCounts
+          totalTodosCount={totalTodosCount}
+          completedTodosCount={completedTodosCount}
+          incompleteTodosCount={incompleteTodosCount}
+        />
+      </div>
     </div>
   );
 }
